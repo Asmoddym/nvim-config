c7d26ef2dcfef792f20df39a89a47d6d2f3bc98b
Asmoddym <asmoddym@gmail.com>
2025-04-19 16:41:01 +0200

    Tests

--- lua/config/command.lua
+++ lua/config/command.lua
@@ -1,4 +1,79 @@
 -- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
+-- Rails specs
 
 
 vim.api.nvim_create_user_command("SpecRunThisExample", function()
@@ -8,3 +83,12 @@ vim.api.nvim_create_user_command("SpecRunThisExample", function()
 	local command = "RAILS_ENV=test HEADLESS=false bundle exec rspec"
 	vim.cmd("vertical terminal " .. command .. " " .. path .. ":" .. line)
 end, {desc = "Spec: run this particular example" })
+
+vim.api.nvim_create_user_command("GitFloatingDiff", function()
+	buffer = vim.api.nvim_create_buf(true, true)
+	win = vim.api.nvim_open_win(buffer, true, {relative = 'win', row = 10, col = 10, height = 50, width = 100, border = 'rounded' , focusable= true})
+	-- vim.cmd(":Gedit :")
+	-- vim.fn.win_execute(win, ":!git diff")
+	vim.fn.win_execute(win, ":terminal git diff")
+	vim.api.nvim_input("i")
+end, {})
