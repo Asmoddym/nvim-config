f77de603da551ac638f3dfc067deb384a7b67286
Sylvain CHAUGNY <sylvain.chaugny@elevo.io>
2025-04-21 18:23:53 +0200

    Fixed tmux command

--- lua/commands/rails.lua
+++ lua/commands/rails.lua
@@ -1,14 +1,18 @@
 utils = require('commands.utils')
 
+function run_spec_through_tmux(path, line)
+  local win = utils.create_float(0.8, 0.8, ":terminal tmux new -A -stest-console -d -- \"cd $ELEVO_ROOT_PATH && RUN_TEST_IN_CONSOLE=true bundle exec rails c -e test\"")
+
+  local cmd = "i" .. utils.termcode("<CR>") .. "run_spec \"" .. path .. "\", " .. line .. utils.termcode("<CR>") .. utils.termcode("<ESC>")
+  vim.api.nvim_feedkeys(cmd, 'm', false)
+end
+
 vim.api.nvim_create_user_command("RailsSpecRunThisExample", function(args)
-	path = vim.fn.expand("%")
+  path = vim.fn.expand("%")
 	line = vim.fn.line(".")
 
 	if args["fargs"][1] == "-tmux" then
-		local win = utils.create_float(0.8, 0.8, ":terminal tmux attach -t tests")
-
-		local cmd = "i" .. utils.termcode("<CR>") .. "run_spec \"" .. path .. "\", " .. line .. utils.termcode("<CR>") .. utils.termcode("<ESC>")
-		vim.api.nvim_feedkeys(cmd, 'm', false)
+    run_spec_through_tmux(path, line)
 	else
 		local command = "RAILS_ENV=test HEADLESS=false bundle exec rspec"
 
