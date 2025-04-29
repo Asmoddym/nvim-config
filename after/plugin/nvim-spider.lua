a3be43f79be53b95864b254745bb86bb6d9baa00
Asmoddym <asmoddym@gmail.com>
2025-04-29 21:34:35 +0200

    Fixed some stuff on maps

--- after/plugin/nvim-spider.lua
+++ after/plugin/nvim-spider.lua
@@ -2,7 +2,6 @@ vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<C
 vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>")
 vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>")
 
--- -- default values
--- require("spider").setup {
--- 	skipInsignificantPunctuation = false
--- }
+require("spider").setup {
+	skipInsignificantPunctuation = false
+}
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fg', ":Telescope live_grep<CR>i", { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = 'Telescope old files' })
@@ -10,7 +10,8 @@ local project_actions = require("telescope._extensions.project.actions")
 
 telescope.setup {
 	defaults = {
-		file_ignore_patterns = {  ".git\\", ".git/" }
+		file_ignore_patterns = {  ".git\\", ".git/" },
+    initial_mode = "normal"
 	},
 	pickers = {
 		find_files = {
