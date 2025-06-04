5a8dc74f0a7531a9f0b05be1172aa05c5321fc53
Sylvain CHAUGNY <sylvain.chaugny@elevo.io>
2025-06-04 20:56:06 +0200

    Copilot

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -4,3 +4,18 @@ vim.keymap.set('i', '<S-TAB>', 'copilot#Accept("\\<CR>")', {
 })
 vim.g.copilot_no_tab_map = true
 
+
+vim.g.copilot_filetypes = {
+  ['*'] = false,
+  ['javascript'] = true,
+  ['typescript'] = true,
+  ['typescriptreact'] = true,
+  ['typescript.tsx'] = true,
+  ['javascriptreact'] = true,
+  ['json'] = true,
+  ['jsonc'] = true,
+  ['markdown'] = true,
+  ['html'] = true,
+  ['ruby'] = true,
+  ['lua'] = true
+}
