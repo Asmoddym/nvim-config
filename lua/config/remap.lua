2df9591dac1e8fe07d0626d0d06a3a1452fdd5e0
Sylvain Chaugny <sylvain.chaugny@gmail.com>
2025-07-11 12:21:01 +0200

    Remap

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -34,5 +34,5 @@ vim.keymap.set("v", "<leader>d", "\"_d")
 -- vim.keymap.set("v", "<leader>s", "y:%s/<C-r>\"//gI<left><left><Left>\"")
 vim.keymap.set("n", "<leader>S", ":%s/<C-r><C-w>//gI<Left><Left><Left> <C-BS>")
 vim.keymap.set("n", "<leader>s", ":s/<C-r><C-w>//gI<Left><Left><Left> <C-BS>")
-vim.keymap.set("v", "<leader>s", "y:%s/<C-r>\"//gI<left><left><Left> <C-BS>")
+vim.keymap.set("v", "<leader>S", "y:%s/<C-r>\"//gI<left><left><Left> <C-BS>")
 
