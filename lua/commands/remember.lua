cb92d63a2a0dd2931eed15fc0c42ca759c4863a4
Sylvain Chaugny <sylvain.chaugny@gmail.com>
2025-07-19 21:24:59 +0200

    Fixed remember for windows

--- lua/commands/remember.lua
+++ lua/commands/remember.lua
@@ -1,7 +1,13 @@
 local utils = require('commands.utils')
 
 vim.api.nvim_create_user_command("Remember", function()
-  local file = os.getenv("HOME") .. "/.nvim-remember.md"
+  local home = os.getenv("HOME")
+
+  if home == nil then
+    home = os.getenv("REMEMBER_HOME")
+  end
+
+  local file = home .. "/.nvim-remember.md"
 
   utils.create_float(0.7, 0.7, ":e " .. file)
 end, {})
