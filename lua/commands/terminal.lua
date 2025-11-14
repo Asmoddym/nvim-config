a9f09a46dadb583ffbb88c48c54147f19cbea893
Sylvain Chaugny <sylvain.chaugny@elevo.io>
2025-11-14 10:02:28 +0100

    Added some terminal stuff

--- lua/commands/terminal.lua
+++ lua/commands/terminal.lua
@@ -1,5 +1,13 @@
 local utils = require('commands.utils')
 
-vim.api.nvim_create_user_command("TmuxTerminal", function(args)
+vim.api.nvim_create_user_command("TmuxTerminalWindowed", function(args)
   utils.create_float(0.8, 0.8, ":terminal tmux new -A -sterminal-tmux")
 end, {})
+
+vim.api.nvim_create_user_command("TmuxTerminal", function(args)
+  vim.cmd(":terminal tmux new -A -sterminal-tmux")
+end, {})
+
+
+vim.keymap.set("n", "<leader>T", ":TmuxTerminalWindowed<CR>i")
+vim.keymap.set("n", "<leader>t", ":TmuxTerminal<CR>i")
