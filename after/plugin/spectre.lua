e00b529f2ab747eefbb0c17613c581b240800236
Sylvain Chaugny <sylvain.chaugny@gmail.com>
2025-11-17 00:53:31 +0100

    Slightly improved replace keymaps (in theory)

--- after/plugin/spectre.lua
+++ after/plugin/spectre.lua
@@ -3,7 +3,7 @@ require("spectre").setup()
 vim.keymap.set('n', '<leader>sS', '<cmd>lua require("spectre").toggle()<CR>', {
     desc = "Toggle Spectre"
 })
-vim.keymap.set('n', '<leader>ss', '<cmd>lua require("spectre").open_file_search()<CR>', {
+vim.keymap.set('n', '<leader>sf', '<cmd>lua require("spectre").open_file_search()<CR>', {
     desc = "(sp) Search on current file"
 })
 
@@ -14,7 +14,7 @@ vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual(
   desc = "(sp) Search current word"
 })
 
-vim.keymap.set("n", "<leader>sl", ":s/<C-r><C-w>//gI<Left><Left><Left> <C-BS>", { desc = "replace word in line" })
-vim.keymap.set("n", "<leader>sf", ":%s/<C-r><C-w>//gI<Left><Left><Left> <C-BS>", { desc = "replace word across file" })
-vim.keymap.set("v", "<leader>sf", "y:%s/<C-r>\"//gI<left><left><Left> <C-BS>", { desc = "replace selection across file" })
+vim.keymap.set("n", "<leader>rl", ":s/<C-r><C-w>//gI<Left><Left><Left> <C-BS>", { desc = "replace word in line" })
+vim.keymap.set("n", "<leader>rf", ":%s/<C-r><C-w>//gI<Left><Left><Left> <C-BS>", { desc = "replace word across file" })
+vim.keymap.set("v", "<leader>rf", "y:%s/<C-r>\"//gI<left><left><Left> <C-BS>", { desc = "replace selection across file" })
 
